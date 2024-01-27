38fd0d6218c74d0ca538e6a08d3137b48bd5d61d
Neer Khandor <neerkhandor456@gmail.com>
2024-01-27 18:51:01 +0530

    added basics of typescript

---
+++ intro.js
@@ -0,0 +1,9 @@
+console.log("neer");
+var user = {
+    name: "neer",
+    age: 21,
+    degree: "B.Tech"
+};
+console.log(user.name);
+console.log(user.age);
+console.log(user.degree);
---
+++ intro.ts
@@ -0,0 +1,10 @@
+console.log("neer")
+let user={
+    name:"neer",
+    age:21,
+    degree:"B.Tech"
+}
+console.log(user.name)
+console.log(user.age)
+console.log(user.degree)
+// console.log(user.email) property email does not exist
